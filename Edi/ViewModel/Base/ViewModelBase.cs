cfbe948a11b70ec1ec3e61717be228f8f2a5e843
Rio <4218831@qq.com>
2021-05-22 12:51:17 +0800

    Add project files.

---
+++ Edi/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Edi.ViewModel.Base
+{
+    using System.ComponentModel;
+
+    class ViewModelBase : INotifyPropertyChanged
+    {
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
